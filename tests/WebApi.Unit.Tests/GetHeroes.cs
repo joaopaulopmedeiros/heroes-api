ad54bb9ee82542a04f3bc77b9cb0f061fba5aaca
Joao-P Medeiros <Joao-P.Medeiros@btgpactual.com>
2022-01-31 13:23:41 -0300

    tests: start unit test projet

---
+++ tests/WebApi.Unit.Tests/GetHeroes.cs
@@ -0,0 +1,15 @@
+using Moq;
+using System;
+using Xunit;
+
+namespace WebApi.Unit.Tests
+{
+    public class GetHeroes
+    {
+        [Fact]
+        public void ItMustGetHeroesWith200StatusCodeIfAnyOnDatabase()
+        {
+            //var mock = new Mock();
+        }
+    }
+}
