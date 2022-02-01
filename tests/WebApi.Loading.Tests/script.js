ac4bd11e4c5103d97157aa96495b2913dcf531d0
Joao-P Medeiros <Joao-P.Medeiros@btgpactual.com>
2022-02-01 18:16:49 -0300

    tests: loading test - 10 virtual users with duration of 30 milliseconds

---
+++ tests/WebApi.Loading.Tests/script.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  vus: 10,
+  duration: '30s',
+};
+
+export default function () {
+  http.get('http://localhost:5000/heroes?perpage=100');
+  sleep(1);
+}
